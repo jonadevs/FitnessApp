f0d062a8ea8e03eea3b1a9436b6d6fa87c826436
jonadevs <jonathan.schubert.dev@gmail.com>
2023-11-03 17:55:38 +0100

    Use Entity Framework Core in controllers

--- FitnessApp.API/Program.cs
+++ FitnessApp.API/Program.cs
@@ -43,6 +43,10 @@ builder.Services.AddDbContext<FitnessAppContext>(
     )
 );
 
+builder.Services.AddScoped<IFitnessAppRepository, FitnessAppRepository>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
